b1bf21f750f1d1566cecdae65b514209406f96cf
Jingwei <jingwei.li@sap.com>
2019-06-02 19:29:27 +0800

    .

--- src/page/Signin.tsx
+++ src/page/Signin.tsx
@@ -1,24 +1,64 @@
 import React from 'react';
 import { Image } from 'semantic-ui-react';
+import Typed from 'typed.js';
 
-const Signin: React.FC = () => {
-    return (
-        <div style={{
-            backgroundImage: 'url(' + require('../res/backimg.png') + ')',
-            backgroundSize: 'cover',
-            width: '100%', height: '100%',
-            position: 'relative'
-        }}>
+export default class Signin extends React.Component {
+
+    input: any = null
+
+    componentDidMount() {
+        new Typed('#title', {
+            strings: ['Mobile AI Portal'],
+            typeSpeed: 20,
+            backSpeed: 0,
+            fadeOut: true,
+            loop: false,
+            cursorChar: ''
+        })
+        let inputingAccount = true
+        this.input.onchange = () => {
+            
+        }
+    }
+
+    render() {
+        return (
             <div style={{
-                border: '1px solid black',
-                position: 'absolute',
-                width: 300, height: 300,
-                right: 100, bottom: 100
+                backgroundColor: 'rgb(210, 220, 230)',
+                width: '100%', height: '100%',
+                position: 'relative'
             }}>
-                <Image src={require('../res/logo.png')} size='small' />
+                <div style={{
+                    width: 350, height: 120,
+                    border: '1px solid gray',
+                    borderRadius: 5,
+                    textAlign: 'center',
+                    position: 'absolute',
+                    top: '50%', marginTop: -60,
+                    left: '50%', marginLeft: -175
+                }}>
+                    <Image src={require('../res/logo.png')} size='mini' style={{
+                        display: 'inline-block',
+                        position: 'relative',
+                        top: -5
+                    }}/>
+                    <h2 id='title' style={{
+                        display: 'inline-block',
+                        fontFamily: 'Comic Sans MS, Verdana',
+                        fontSize: '2em'
+                    }}></h2>
+                    <br/>
+                    <input ref={(e: HTMLInputElement) => this.input = e} placeholder='input your credential' spellCheck={false} style={{
+                        backgroundColor: 'rgb(200, 200, 200, 0.5)',
+                        outline: 'none',
+                        textDecoration: 'none',
+                        border: '1px solid gray',
+                        borderRadius: 5,
+                        width: '60%', padding: '1px 3px',
+                        boxSizing: 'border-box'
+                    }}/>
+                </div>
             </div>
-        </div>
-    )
+        )
+    }
 }
-
-export default Signin;
