b3b54f41d046819dd8ab3e45bb7498cdcc28cc39
Luncert <2725115515@qq.com>
2019-06-13 23:19:23 +0800

    .

--- src/page/sub-page/Settings.tsx
+++ src/page/sub-page/Settings.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Tab, Form, Radio } from "semantic-ui-react";
+import { Tab, Form, Radio, Image, Button, Segment } from "semantic-ui-react";
 import Intl from "../../com/Intl";
 
 export default class Settings extends React.Component {
@@ -22,8 +22,42 @@ export default class Settings extends React.Component {
 
 class ProfileSettings extends React.Component {
 
+    private oldPwd: string;
+    private newPwd: string;
+    private repeatPwd: string;
+
+    save() {
+        alert('saved')
+    }
+
     render() {
-        return <Tab.Pane>Profile</Tab.Pane>;
+        return (
+            <Segment>
+                <Form>
+                    <Form.Group>
+                        <Image src={require('../../res/img/default-avatar.png')} />
+                        <Button compact icon='edit' floated='left'
+                            onClick={() => (this.refs.avatarInput as HTMLInputElement).click() }
+                            style={{
+                                position: 'absolute',
+                                top: 180, left: 180
+                            }} />
+                        <input ref='avatarInput' type='file' style={{display: 'none'}} />
+                    </Form.Group>
+                    <Form.Group widths='equal'>
+                        <Form.Input fluid 
+                            label={Intl.get('Home.Settings.Profile.OldPassword')} />
+                        <Form.Input fluid 
+                            label={Intl.get('Home.Settings.Profile.NewPassword')} />
+                        <Form.Input fluid 
+                            label={Intl.get('Home.Settings.Profile.RepeatPassword')} />
+                    </Form.Group>
+                    <Form.Button compact floated='right' onClick={this.save.bind(this)}>
+                        {Intl.get('Home.Settings.Profile.Save')}
+                    </Form.Button>
+                </Form>
+            </Segment>
+        );
     }
 
 }
@@ -39,7 +73,7 @@ class OtherSettings extends React.Component {
     render() {
         const curLocale = Intl.getCurrentLocale();
         return (
-            <Tab.Pane>
+            <Segment>
                 <Form>
                     <Form.Input fluid type='file' onChange={(e) => this.backgroundImage = e.target.value}
                         label={Intl.get('Home.Settings.Other.BackgroundImage')} />
@@ -64,7 +98,7 @@ class OtherSettings extends React.Component {
                         {Intl.get('Home.Settings.Other.Save')}
                     </Form.Button>
                 </Form>
-            </Tab.Pane>
+            </Segment>
         );
     }
     
